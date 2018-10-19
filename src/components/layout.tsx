664c750fb357d9c6466b5927a55364163efdab57
xandyn <mail.vagapov@gmail.com>
2018-10-19 15:37:33 +0500

    split controls and timer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,48 +1,20 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
 import { Grid } from './grid';
-import { appStore, GameDifficulty, GameState } from '../stores';
+import { Controls } from './controls';
+import { Timer } from './timer';
+import { appStore, GameState } from '../stores';
 
 import './layout.css';
 
 @observer
 export class Layout extends React.Component {
-
-  toggleGameState = () => {
-    if (appStore.gameState === GameState.Started) {
-      appStore.setGameState(GameState.Idle);
-    } else {
-      appStore.setGameState(GameState.Started);
-    }
-  }
-
-  onSelectDifficulty = (event: React.SyntheticEvent<HTMLSelectElement>): void => {
-    const difficulty = event.currentTarget.value;
-    appStore.setGameDifficulty(GameDifficulty[difficulty]);
-  }
-
   render() {
-    const { gameState, remainingTime } = appStore;
+    const { gameState } = appStore;
     return (
       <div className="layout">
-        <div className="controls">
-          <select
-            className="difficulty"
-            name="difficulty"
-            onChange={this.onSelectDifficulty}
-            disabled={gameState === GameState.Started}
-          >
-            {Object.keys(GameDifficulty).map((difficulty) => (
-              <option key={difficulty} value={difficulty}>{difficulty}</option>
-            ))}
-          </select>
-          <button onClick={this.toggleGameState} className="start">
-            {gameState === GameState.Started ? 'Stop Game' : 'Start Game'}
-          </button>
-        </div>
-        <div className="timer">
-          Time left: {remainingTime}s
-        </div>
+        <Controls />
+        <Timer />
         {gameState === GameState.Idle || gameState === GameState.Started ? (
           <Grid />
         ) : gameState === GameState.Win ? (
